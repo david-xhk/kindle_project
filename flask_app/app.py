747e64d8e3bc1b3eb19701685a05ac2c1aacd1c0
han-keong <hk997@live.com>
2020-10-31 03:14:55 +0800

    update render_book

--- flask_app/app.py
+++ flask_app/app.py
@@ -15,15 +15,21 @@ def hello_world():
 @app.route("/book/<asin>")
 def get_book(asin):
     book = mongo.db.kindle_metadata.find_one_or_404({ "asin" : asin })
-    return book_template(book)
+    return render_book(book)
 
-def book_template(book):
+def render_book(book):
+    related = book.get("related") or {}
     return render_template("book.html",
-        asin = book["asin"],
-        title = book.get("title", "Untitled"),
-        imUrl = book.get("imUrl", "https://images-na.ssl-images-amazon.com/images/I/61Ww4abGclL._AC_SX425_.jpg"),
-        description = book.get("description", "No description available"),
-        buy_after_viewing = (book.get("related") or {}).get("buy_after_viewing"))
+        asin              = book["asin"],
+        title             = book.get("title", "Untitled"),
+        imUrl             = book.get("imUrl", "https://images-na.ssl-images-amazon.com/images/I/61Ww4abGclL._AC_SX425_.jpg"),
+        description       = book.get("description", "Description not available"),
+        categories        = book.get("categories", []),
+        also_viewed       = related.get("also_viewed"),
+        also_bought       = related.get("also_bought"),
+        buy_after_viewing = related.get("buy_after_viewing"),
+        price             = book.get("price", "Price not available"),
+        salesRank         = book.get("salesRank", "Sales rank not available"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
