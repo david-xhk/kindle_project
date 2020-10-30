e4db5d88914c2767dfd769e64ae71bdd9454f7e0
han-keong <hk997@live.com>
2020-10-31 00:20:05 +0800

    flask app v0.1

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, render_template
 from config import Config
 from flask_pymongo import PyMongo
 
@@ -6,23 +6,24 @@ app = Flask(__name__)
 app.config.from_object(Config)
 mongo = PyMongo(app)
 
-@app.route("/")
+@app.route("/random/book")
 def hello_world():
     book = next(mongo.db.kindle_metadata.aggregate([{ "$sample" : { "size": 1 } }]))
     asin = book["asin"]
-    title = book.get("title", "Untitled")
-    imUrl = book.get("imUrl", "https://images-na.ssl-images-amazon.com/images/I/61Ww4abGclL._AC_SX425_.jpg")
-    description = book.get("description", "No description available")
+    return redirect(url_for("get_book", asin=asin))
 
-    return f"""\
-<html>
-    <h1>{title}</h1>
-    <img src="{imUrl}" alt="{title}" />
-    <br />
-    <h2>Description</h2>
-    <p>{description}</p>
-    <span><b>ASIN:</b> {asin}</span>
-</html>"""
+@app.route("/book/<asin>")
+def get_book(asin):
+    book = mongo.db.kindle_metadata.find_one_or_404({ "asin" : asin })
+    return book_template(book)
+
+def book_template(book):
+    return render_template("book.html",
+        asin = book["asin"],
+        title = book.get("title", "Untitled"),
+        imUrl = book.get("imUrl", "https://images-na.ssl-images-amazon.com/images/I/61Ww4abGclL._AC_SX425_.jpg"),
+        description = book.get("description", "No description available"),
+        buy_after_viewing = (book.get("related") or {}).get("buy_after_viewing"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
