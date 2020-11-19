8d8945e1a3493bec2d1d9787f6c623b3e6bd61d4
han-keong <hk997@live.com>
2020-11-19 18:43:55 +0800

    updated flask app config

--- flask_app/app/config.py
+++ flask_app/app/config.py
@@ -1,8 +1,16 @@
+import getpass
 
 class Config:
-    MYSQL_HOST = "10.12.192.169"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "password"
-    MYSQL_DB = "test"
-    MYSQL_PORT = 3306
-    MONGO_URI = "mongodb://root:password@10.12.192.169:27017/project?authSource=admin&readPreference=primary&ssl=false"
+    MYSQL_HOST = input("MySQL host: ") or "127.0.0.1",
+    MYSQL_PORT = input("MySQL port: ") or 3306
+    MYSQL_DB = input("MySQL database: ") or "project"
+    MYSQL_USER = input("MySQL user: ") or "root"
+    MYSQL_PASSWORD = getpass.getpass("MySQL password: ") or "password"
+    
+    MONGO_URI = "mongodb://{user}:{password}@{host}:{port}/{db}?authSource=admin&readPreference=primary&ssl=false".format(
+        host     = input("MongoDB host: ") "127.0.0.1",
+        port     = input("MongoDB port: ") or 27017,
+        db       = input("MongoDB database: ") or "project",
+        user     = input("MongoDB user: ") or "root",
+        password = getpass.getpass("MongoDB password: ") or "password"
+    )
